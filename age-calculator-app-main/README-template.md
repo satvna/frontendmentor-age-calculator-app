e40a3a2b7f771ba6f910f1496fb05cb002137bd2
Sarah Tavana <sarahtvana@gmail.com>
2023-07-12 15:10:09 -0500

    functional calculation and validation

--- age-calculator-app-main/README-template.md
+++ age-calculator-app-main/README-template.md
@@ -1,20 +1,22 @@
 # Frontend Mentor - Age calculator app solution
 
-This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
-- [Overview](#overview)
-  - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
-  - [Links](#links)
-- [My process](#my-process)
-  - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources)
-- [Author](#author)
-- [Acknowledgments](#acknowledgments)
+- [Frontend Mentor - Age calculator app solution](#frontend-mentor---age-calculator-app-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Continued development](#continued-development)
+    - [Useful resources](#useful-resources)
+  - [Author](#author)
+  - [Acknowledgments](#acknowledgments)
 
 **Note: Delete this note and update the table of contents based on what sections you keep.**
 
@@ -41,7 +43,7 @@ Users should be able to:
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
 
 Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
 
@@ -99,10 +101,7 @@ Use this section to outline areas that you want to continue focusing on in futur
 
 ### Useful resources
 
-- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
-- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
-
-**Note: Delete this note and replace the list above with resources that helped you during the challenge. These could come in handy for anyone viewing your solution or for yourself when you look back on this project in the future.**
+- [StackOverflow](https://stackoverflow.com/a/37190274) - I used this solution to do the calculations.
 
 ## Author
 
---
+++ age-calculator-app-main/script.js
@@ -0,0 +1,114 @@
+const timeNow = new Date();
+const validated = false;
+
+function daysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
+}
+
+function styleError(errorId, inputId, labelId){
+  document.getElementById(errorId).style.visibility = "visible";
+  document.getElementById(inputId).style.borderColor = "hsl(0, 100%, 67%)";
+  document.getElementById(labelId).style.color = "hsl(0, 100%, 67%)";
+}
+function unStyleError(errorId, inputId, labelId){
+  document.getElementById(errorId).style.visibility = "hidden";
+  document.getElementById(inputId).style.borderColor = "hsl(0, 1%, 44%)";
+  document.getElementById(labelId).style.color = "hsl(0, 1%, 44%)";
+}
+
+function validate(day, month, year){
+  //validate empty inputs
+  if (day.length == 0){
+    styleError('dayerr', 'day-input', 'day-label');
+    return false;
+  }
+  else {
+    unStyleError('dayerr', 'day-input', 'day-label');
+  }
+  if (month.length == 0){
+    styleError('montherr', 'month-input', 'month-label');
+    return false;
+  }
+  else {
+    unStyleError('montherr', 'month-input', 'month-label');
+  }
+  if (year.length == 0){
+    styleError('yearerr', 'year-input', 'year-label');
+    return false;
+  }
+  else {
+    unStyleError('yearerr', 'year-input', 'year-label');
+  }
+
+  //validate out of bounds inputs
+  if (parseInt(day) > 31){
+    styleError('dayerr', 'day-input', 'day-label');
+    document.getElementById("dayerr").textContent = "Must be a valid day";
+    return false;
+  }
+  else {
+    unStyleError('dayerr', 'day-input', 'day-label');
+  }
+  if (parseInt(month) > 12){
+    document.getElementById("montherr").textContent = "Must be a valid month";
+    styleError('montherr', 'month-input', 'month-label');
+    return false;
+  }
+  else {
+    unStyleError('montherr', 'month-input', 'month-label');
+  }
+  if (parseInt(year) > timeNow.getFullYear()){
+    document.getElementById("yearerr").textContent = "Must be in the past";
+    styleError('yearerr', 'year-input', 'year-label');
+    return false;
+  }
+  else {
+    unStyleError('yearerr', 'year-input', 'year-label');
+  }
+
+  //validate months with a weird number of days in the year
+  if (parseInt(day) > daysInMonth(month, year)){
+    document.getElementById("dayerr").textContent = "Must a valid date";
+    styleError('dayerr', 'day-input', 'day-label');
+    return false;
+
+  }
+  else {
+    unStyleError('dayerr', 'day-input', 'day-label');
+  }
+  return true;
+}
+
+function getNewTime(){
+  let day = document.getElementById('day-input').value;
+  let month = document.getElementById('month-input').value;
+  let year = document.getElementById('year-input').value;
+
+  let isValidated = validate(day, month, year);
+  if (isValidated){
+    let timeTarget = new Date(year, (month-1), day);
+
+    let yearsElapsed = Math.round(timeNow.getFullYear() - timeTarget.getFullYear());
+    let monthsElapsed = timeNow.getMonth() - timeTarget.getMonth();
+    let daysElapsed = timeNow.getDate() - timeTarget.getDate();
+    if (monthsElapsed < 0){
+      yearsElapsed -= 1;
+      monthsElapsed = 12 + monthsElapsed;
+    }
+    if (daysElapsed < 0) {
+      monthsElapsed -= 1;
+      daysElapsed = 30 + daysElapsed;
+
+      var copy1 = new Date(timeTarget.getTime());
+      copy1.setDate(32);
+      daysElapsed = 32-timeTarget.getDate()-copy1.getDate()+timeNow.getDate();
+
+    }
+
+    document.getElementById("year-num").textContent = yearsElapsed;
+    document.getElementById("month-num").textContent = monthsElapsed;
+    document.getElementById("day-num").textContent = daysElapsed;
+  }
+
+
+}
